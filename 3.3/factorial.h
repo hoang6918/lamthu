f2e4c119dd4d951f6be74fa557dd4dec6e150087
hoang6918 <80245864+hoang6918@users.noreply.github.com>
2022-10-06 09:45:02 +0700

    Delete 3.3 directory

--- 3.3/factorial.h
+++
@@ -1,6 +0,0 @@
-// factorial.h
-#ifndef FACTORIAL_H
-#define FACTORIAL_H
-int factorial (const int aNUmber);
-
-#endif
--- 3.3/readline.h
+++
@@ -1,7 +0,0 @@
-// readline.h
-#ifndef READ_LINE_H
-#define READ_LINE_h
-
-int read_line (char *str);
-
-#endif
